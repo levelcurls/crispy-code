56d0fe335e5325dcec8c71995cfc34d6f37ca1bf
Aileen Myron <165148368+levelcurls@users.noreply.github.com>
2024-03-27 11:37:11 +0200

    Create mainso.js

---
+++ mainso.js
@@ -0,0 +1,24 @@
+ exports.burrowsWheeler.encode = function(str) {
+    str = '$' + str;
+    var combinations = [];
+    for (let i = 0; i < str.length; i += 1) {
+      combinations.push(str.substring(i) + str.substring(0, i));
+    }
+    var sorted = combinations.sort();
+    var result = [];
+    for (let i = 0; i < sorted.length; i += 1) {
+      result.push(combinations[i][str.length - 1]);
+    }
+    return result.join('');
+  }
+
+  exports.burrowsWheeler.decode = function(encodedStr) {
+    const sortedCharSequence = encodedStr.split('').sort().join('');
+    const leftSide = {};
+    const rightSide = {};
+    var maxEachCharLeft = {};
+    var maxEachCharRight = {};
+
+    for (let i = 0; i < encodedStr.length; i += 1) {
+      var idLeft = sortedCharSequence[i];
+      if (idLeft in maxEachCharLeft) {
